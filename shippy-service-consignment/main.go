c7424bc93107bbdaf12117a9c28f69dbc4e5c618
林添耀 <lintianyao@outlook.com>
2019-11-11 11:32:38 +0800

    修改为容器编译

--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -12,10 +12,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"sync"
 
 	// Import the generated protobuf code
 	pb "github.com/lty5240/consignment/shippy-service-consignment/proto/consignment"
+	vesselProto "github.com/lty5240/consignment/shippy-service-vessel/proto/vessel"
 	"github.com/micro/go-micro"
 )
 
@@ -50,13 +52,26 @@ func (repo *Repository) GetAll() []*pb.Consignment {
 // in the generated code itself for the exact method signatures etc
 // to give you a better idea.
 type service struct {
-	repo repository
+	repo         repository
+	vesselClient vesselProto.VesselServiceClient
 }
 
 // CreateConsignment - we created just one method on our service,
 // which is a create method, which takes a context and a request as an
 // argument, these are handled by the gRPC server.
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+		MaxWeight: req.Weight,
+		Capacity:  int32(len(req.Containers)),
+	})
+	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
+	if err != nil {
+		return err
+	}
+
+	// We set the VesselId as the vessel we got back from our
+	// vessel service
+	req.VesselId = vesselResponse.Vessel.Id
 
 	// Save our consignment
 	consignment, err := s.repo.Create(req)
@@ -89,9 +104,10 @@ func main() {
 
 	// Init will parse the command line flags.
 	srv.Init()
+	vesselClient := vesselProto.NewVesselServiceClient("shippy.service.vessel", srv.Client())
 
 	// Register handler
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
 
 	// Run the server
 	if err := srv.Run(); err != nil {
---
+++ shippy-service-vessel/go.mod
@@ -0,0 +1,9 @@
+module github.com/lty5240/consignment/shippy-service-vessel
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/micro/go-micro v1.15.1
+	golang.org/x/net v0.0.0-20191109021931-daa7c04131f5
+)
